4caf664d0bdc51ccf0e01114f8e48f51e1dbe0da
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-10-11 19:41:37 -0400

    Finish writing Turn tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -9,21 +9,35 @@ describe('Turn', function(){
     var turn = new Turn();
     assert.isFunction(turn);
   })
-  it('should be an instance of Turn', function(){
+  it.skip('should be an instance of Turn', function(){
     var turn = new Turn();
     assert.instanceOf(turn, Turn);
   })
-  it('should be able to return a guess', function(){
+  it.skip('should be able to return a guess', function(){
     var card = new Card(1, `What's for dinner tonight?`, ['burgers', 'spaghetti', 'curry'], 'curry');
     var turn = new Turn('burgers', card);
     assert.equal(turn.returnGuess(), 'burgers');
   })
-  it('should be able to return a card', function(){
+  it.skip('should be able to return a card', function(){
     var card = new Card(1, `What's for dinner tonight?`, ['burgers', 'spaghetti', 'curry'], 'curry');
     var turn = new Turn('burgers', card);
     assert.equal(turn.returnCard(), card);
   })
-  // evaluateGuess method returns a boolean indicating correct or not HAPPY/SAD
-  // giveFeedback returns "correct!"/"incorrect!" based on prev. HAPPY/SAD
+  it.skip('should be able to evaluate whether a guess is correct or not', function() {
+    var card = new Card(1, `What's for dinner tonight?`, ['burgers', 'spaghetti', 'curry'], 'curry');
+    var turn = new Turn('burgers', card);
+    assert.equal(turn.evaluateGuess(), false);
 
+    var turn2 = new Turn('curry', card);
+    assert.equal(turn2.evaluateGuess(), true);
+  })
+  it.skip('should be able to relay to the user whether they\'re correct', function(){
+    var card = new Card(1, `What's for dinner tonight?`, ['burgers', 'spaghetti', 'curry'], 'curry');
+    var turn = new Turn('burgers', card);
+    assert.equal(turn.giveFeedback(), "incorrect!");
+
+    var turn2 = new Turn('curry', card);
+    assert.equal(turn2.giveFeedback(), "correct!");
+
+  })
 });
