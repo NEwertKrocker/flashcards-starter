8bbfde23187c547958f06bb9edadedabd5765452
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-10-13 16:19:33 -0400

    Finish Round functionality and test

--- src/Round.js
+++ src/Round.js
@@ -5,6 +5,7 @@ class Round {
     this.deck = deck;
     this.currentCard = this.deck.cards[0];
     this.turns = 0;
+    this.incorrectGuesses = [];
   };
   returnCurrentCard(){
     return this.currentCard;
@@ -12,9 +13,19 @@ class Round {
   takeTurn(guess){
     var turn = new Turn(guess, this.currentCard);
     var currentCardIndex = this.deck.cards.indexOf(this.currentCard);
+    if(!turn.evaluateGuess()){
+      this.incorrectGuesses.push(this.currentCard.id);
+    }
     this.turns++;
     this.currentCard = this.deck.cards[currentCardIndex + 1];
-    return turn;
+    return turn.giveFeedback();
+  }
+  calculatePercentCorrect(){
+    var percentCorrect = (this.deck.cards.length - this.incorrectGuesses.length);
+    return Math.floor((percentCorrect / this.deck.cards.length) * 100);
+  }
+  endRound(){
+    return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
   }
 };
 
