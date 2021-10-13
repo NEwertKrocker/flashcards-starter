d8d129d87f033655219df5ad4c6e6104545c41d7
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-10-13 10:29:56 -0400

    Add ability for Round class to increment currentCard

--- src/Round.js
+++ src/Round.js
@@ -3,15 +3,19 @@ const Turn = require('../src/Turn');
 class Round {
   constructor(deck){
     this.deck = deck;
-    this.currentCard = this.deck[0];
+    this.currentCard = this.deck.cards[0];
+    this.turns = 0;
   };
   returnCurrentCard(){
     return this.currentCard;
   }
   takeTurn(guess){
     var turn = new Turn(guess, this.currentCard);
-    
+    var currentCardIndex = this.deck.cards.indexOf(this.currentCard);
+    this.turns++;
+    this.currentCard = this.deck.cards[currentCardIndex + 1];
+    return turn;
   }
 };
 
-module.exports = Round, Turn;
+module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -15,6 +15,7 @@ describe('Round', function(){
 
     assert.isFunction(Round);
   });
+
   it('should be an instance of Round', function() {
     var card1 = new Card(1, "What has it got in its pocketses?", ["string", "loose change", "a Ring of Power"], 'a Ring of Power');
     var card2 = new Card(4, "What's new pussycat?", ["not much", "got a haircut", "got my doctorate"], 'got a haircut');
@@ -23,34 +24,69 @@ describe('Round', function(){
 
     assert.instanceOf(round, Round);
   });
+
   it('should begin with a currentCard, which is the first card in the Deck', function(){
     var card1 = new Card(1, "What has it got in its pocketses?", ["string", "loose change", "a Ring of Power"], 'a Ring of Power');
     var card2 = new Card(4, "What's new pussycat?", ["not much", "got a haircut", "got my doctorate"], 'got a haircut');
     var deck = new Deck([card1, card2]);
     var round = new Round(deck);
 
-    assert.equal(round.currentCard, round.deck[0]);
+    assert.equal(round.currentCard, round.deck.cards[0]);
   });
+
   it('should be able to return the current card', function(){
     var card1 = new Card(1, "What has it got in its pocketses?", ["string", "loose change", "a Ring of Power"], 'a Ring of Power');
     var card2 = new Card(4, "What's new pussycat?", ["not much", "got a haircut", "got my doctorate"], 'got a haircut');
     var deck = new Deck([card1, card2]);
     var round = new Round(deck);
 
-    assert.equal(round.returnCurrentCard(), round.deck[0]);
+    assert.equal(round.returnCurrentCard(), round.deck.cards[0]);
   });
+
   it('should be able to create new Turn instances when a guess is made', function(){
     var card1 = new Card(1, "What has it got in its pocketses?", ["string", "loose change", "a Ring of Power"], 'a Ring of Power');
     var card2 = new Card(4, "What's new pussycat?", ["not much", "got a haircut", "got my doctorate"], 'got a haircut');
     var deck = new Deck([card1, card2]);
     var round = new Round(deck);
 
-    assert.exists(turn);
+    var turn = round.takeTurn('string');
 
+    assert.instanceOf(turn, Turn);
+    assert.equal(turn.guess, 'string');
   });
 
-  // it should update the turn count when a guess is made
-  // it should queue up the next card
+  it('should update the turn count when a guess is made', function(){
+    var card1 = new Card(1, "What has it got in its pocketses?", ["string", "loose change", "a Ring of Power"], 'a Ring of Power');
+    var card2 = new Card(4, "What's new pussycat?", ["not much", "got a haircut", "got my doctorate"], 'got a haircut');
+    var deck = new Deck([card1, card2]);
+    var round = new Round(deck);
+
+    assert.equal(round.turns, 0);
+
+    round.takeTurn();
+
+    assert.equal(round.turns, 1);
+
+    round.takeTurn();
+    round.takeTurn();
+
+    assert.equal(round.turns, 3);
+  });
+  it('should queue up the next card when a turn is taken', function(){
+    var card1 = new Card(1, "What has it got in its pocketses?", ["string", "loose change", "a Ring of Power"], 'a Ring of Power');
+    var card2 = new Card(4, "What's new pussycat?", ["not much", "got a haircut", "got my doctorate"], 'got a haircut');
+    var deck = new Deck([card1, card2]);
+    var round = new Round(deck);
+
+    assert.equal(round.turns, 0);
+    assert.equal(round.currentCard, round.deck.cards[0]);
+
+    round.takeTurn();
+
+    assert.equal(round.turns, 1);
+    assert.equal(round.currentCard, round.deck.cards[1]);
+  });
+  
   // it should record whether the guess is correct or incorrect
   // it should store incorrect guesses in an array
   // it should return feedback if the guess is correct
